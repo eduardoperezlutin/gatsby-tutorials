ce5833939a3d0597c1cc7b463b5d5af4fe9d4533
eduardoperezlutin <eduardoperezlutin@gmail.com>
2019-04-28 14:02:01 -0600

    Typography Plugin Installation | Layout definition and reutilization in some pages | Develop navigation using Link from Gatsby

---
+++ 3_Nested-Layout-Components/src/components/listLink.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export const ListLink = props => (
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
